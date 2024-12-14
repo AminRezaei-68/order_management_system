f27b0dab08e3b9bdfa9ec2fe6c92d805d7f73f52
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-15 00:00:17 +0330

    create service for findAll and findOne and create user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+  ) {}
+
+  private async hashPassword(password: string) {
+    const salt = await bcrypt.genSalt(10);
+    return bcrypt.hash(password, salt);
+  }
+
+  async findAll() {
+    return this.userModel.find();
+  }
+
+  async findOne(id: string) {
+    return this.userModel.findById(id);
+  }
+
+  async create(createUserDto: CreateUserDto) {
+    const { email, password } = createUserDto;
+    const existingUser = await this.userModel.findOne({ email: email }).exec();
+
+    if (existingUser) {
+      throw new BadRequestException('A user with this email already exists.');
+    }
+
+    const hashedPassword = await this.hashPassword(password);
+    const user = new this.userModel({ ...createUserDto, hashedPassword });
+
+    const savedUser = await user.save();
+
+    return {
+      id: savedUser._id.toString(),
+      name: savedUser.name,
+      email: savedUser.email,
+      username: savedUser.username,
+    };
+  }
+}
