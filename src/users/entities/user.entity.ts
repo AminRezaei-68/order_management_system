9004624ba7a72ba31383fdac7c110efc814d4062
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-14 20:40:57 +0330

    change the name of user entity to user scheam

--- src/users/entities/user.entity.ts
+++
@@ -1,7 +0,0 @@
-export class User {
-  id: string;
-  name: string;
-  username: string;
-  eamil: string;
-  password: string;
-}
---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,27 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { UserInterface } from '../interfaces/user.interface';
+
+@Schema({ timestamps: true })
+export class User extends Document {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true, unique: true })
+  username: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ type: [String], default: ['user'] })
+  roles: string[];
+
+  @Prop({ default: true })
+  isActive: boolean;
+}
+
+export type UserDocument = User & Document;
+
+export const userSchema = SchemaFactory.createForClass(User);
