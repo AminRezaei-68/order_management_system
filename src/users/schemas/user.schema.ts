79036d77c19d21b6ed45a6a30617aae95cbc8e59
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-14 22:01:58 +0330

    upgrade user schema

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { UserInterface } from '../interfaces/user.interface';
+import { Document } from 'mongoose';
 
 @Schema({ timestamps: true })
 export class User extends Document {
