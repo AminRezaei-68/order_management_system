2bc4746e3899e0a581d86906a86b4ca8a6f694d7
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-14 22:00:34 +0330

    add config to connecting to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { OrdersModule } from './orders/orders.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule, ProductsModule, UsersModule, OrdersModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/order-management'),
+    AuthModule,
+    ProductsModule,
+    UsersModule,
+    OrdersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
