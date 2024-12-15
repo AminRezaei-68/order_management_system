b3adb94c4bee550104814ca14893d3e91279fb9f
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-15 22:19:33 +0330

    add method to update status in user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import {
   BadRequestException,
   Injectable,
   NotFoundException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { User } from './schemas/user.schema';
 import { Model } from 'mongoose';
@@ -9,6 +10,8 @@ import { InjectModel } from '@nestjs/mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import * as bcrypt from 'bcrypt';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ChangePasswordDto } from './dto/change-password.dto';
+import { UpdateUserStatusDto } from './dto/update-user-status.dto';
 
 @Injectable()
 export class UsersService {
@@ -19,6 +22,46 @@ export class UsersService {
   private async hashPassword(password: string) {
     const salt = await bcrypt.genSalt(10);
     return bcrypt.hash(password, salt);
+  }
+
+  async changePassword(userId: string, changePasswordDto: ChangePasswordDto) {
+    const user = await this.userModel.findById(userId);
+    if (!user) {
+      throw new NotFoundException(`The user with id: ${userId} is not exist.`);
+    }
+
+    const { currentPassword, newPassword } = changePasswordDto;
+
+    const isMatch = await bcrypt.compare(currentPassword, user.password);
+
+    if (!isMatch) {
+      throw new UnauthorizedException('Current password is incorrect');
+    }
+
+    const hashedPassword = await bcrypt.hashPassword(newPassword);
+    user.password = hashedPassword;
+
+    await user.save();
+
+    return { message: 'Password updated successfully' };
+  }
+
+  async updateStatus(userId: string, updateUserStatusDto: UpdateUserStatusDto) {
+    const user = await this.userModel.findById(userId);
+
+    if (!user) {
+      throw new NotFoundException(`User with ID ${userId} not found`);
+    }
+
+    const { isActive } = updateUserStatusDto;
+
+    user.isActive = isActive;
+
+    await user.save();
+
+    return {
+      message: `User status is changed to ${isActive ? 'active' : 'Inactive'}`,
+    };
   }
 
   async findAll() {
@@ -95,10 +138,6 @@ export class UsersService {
           'A user with this username already exists.',
         );
       }
-    }
-
-    if (updateUserDto.password) {
-      updateUserDto.password = await this.hashPassword(updateUserDto.password);
     }
 
     Object.assign(user, updateUserDto);
