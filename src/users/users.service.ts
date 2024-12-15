516ebe25878bb628cddb4fb709f46a9033dc3f0a
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-15 18:28:13 +0330

    add update and remove method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,6 +8,7 @@ import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import * as bcrypt from 'bcrypt';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -29,15 +30,30 @@ export class UsersService {
   }
 
   async create(createUserDto: CreateUserDto) {
-    const { email, password } = createUserDto;
+    const { email, password, username } = createUserDto;
     const existingUser = await this.userModel.findOne({ email: email }).exec();
+
+    const existingusername = await this.userModel
+      .findOne({
+        username: username,
+      })
+      .exec();
+
+    if (existingusername) {
+      throw new BadRequestException(
+        'A user with this username already exists.',
+      );
+    }
 
     if (existingUser) {
       throw new BadRequestException('A user with this email already exists.');
     }
 
     const hashedPassword = await this.hashPassword(password);
-    const user = new this.userModel({ ...createUserDto, hashedPassword });
+    const user = new this.userModel({
+      ...createUserDto,
+      password: hashedPassword,
+    });
 
     const savedUser = await user.save();
 
@@ -47,5 +63,61 @@ export class UsersService {
       email: savedUser.email,
       username: savedUser.username,
     };
+  }
+
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    const user = await this.userModel.findById(id).exec();
+
+    if (!user) {
+      throw new NotFoundException(`The user with id: ${id} is not exist.`);
+    }
+
+    if (updateUserDto.email) {
+      const userExists = await this.userModel
+        .findOne({
+          email: updateUserDto.email,
+        })
+        .exec();
+      if (userExists && userExists.id === id) {
+        throw new BadRequestException('This email is already in use.');
+      }
+    }
+
+    if (updateUserDto.username) {
+      const existingusername = await this.userModel
+        .findOne({
+          username: updateUserDto.username,
+        })
+        .exec();
+
+      if (existingusername) {
+        throw new BadRequestException(
+          'A user with this username already exists.',
+        );
+      }
+    }
+
+    if (updateUserDto.password) {
+      updateUserDto.password = await this.hashPassword(updateUserDto.password);
+    }
+
+    Object.assign(user, updateUserDto);
+    const updatedUser = await user.save();
+
+    return {
+      id: updatedUser._id.toString(),
+      username: updatedUser.username,
+      email: updatedUser.email,
+    };
+  }
+
+  async remove(id: string) {
+    const user = await this.userModel.findByIdAndDelete(id);
+
+    if (!user) {
+      throw new NotFoundException(`The user with id: ${id} is not exist.`);
+    }
+
+    return { message: `User with ID "${id}" has been deleted.` };
   }
 }
