42868ce715eef96df998701fdebc7d156577d155
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-15 22:27:40 +0330

    add ApiTags to usercontroller for Classification and documentation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,7 +13,9 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { ChangePasswordDto } from './dto/change-password.dto';
 import { UpdateUserStatusDto } from './dto/update-user-status.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
