92ee75065bf49eff963ec72e8973a7133c6c8be0
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-16 22:37:37 +0330

    upgrade create user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,36 +1,3 @@
-import {
-  IsBoolean,
-  IsEmail,
-  IsNotEmpty,
-  IsString,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
+import { BaseUserDto } from 'src/common/dtos/base-user.dto';
 
-export class CreateUserDto {
-  @IsString()
-  @IsNotEmpty()
-  @MaxLength(20, { message: 'The name can not be more than 20 character.' })
-  name: string;
-
-  @IsString()
-  @IsNotEmpty()
-  @MaxLength(20, { message: 'The username can not be more than 20 character.' })
-  username: string;
-
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(8, { message: 'The password atleast should have 8 character.' })
-  password: string;
-
-  @IsString()
-  @IsNotEmpty()
-  @IsEmail()
-  email: string;
-
-  @IsString()
-  roles: string[];
-
-  @IsBoolean()
-  isActive: boolean;
-}
+export class CreateUserDto extends BaseUserDto {}
