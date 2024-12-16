9db1315f8af432abf65d2c263ee53266fa7647d5
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-16 22:36:47 +0330

    upgrade change password dto

--- src/users/dto/change-password.dto.ts
+++ src/users/dto/change-password.dto.ts
@@ -1,13 +1,21 @@
-import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsNotEmpty, IsString, Matches, MinLength } from 'class-validator';
 
 export class ChangePasswordDto {
   @IsString()
   @IsNotEmpty()
-  @MinLength(8)
+  @MinLength(8, {
+    message: 'Current password must be at least 8 characters.',
+  })
   currentPassword: string;
 
   @IsString()
   @IsNotEmpty()
-  @MinLength(8)
+  @MinLength(8, {
+    message: 'New password must be at least 8 characters.',
+  })
+  @Matches(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/, {
+    message:
+      'The new password must contain at least one uppercase letter, one lowercase letter, and one number.',
+  })
   newPassword: string;
 }
