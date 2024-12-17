c6b4123a87812a03703066d9d32945b6c71ac0f3
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-17 22:59:59 +0330

    add guards to user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
   Patch,
   Post,
   Req,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -14,28 +15,36 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { ChangePasswordDto } from './dto/change-password.dto';
 import { UpdateUserStatusDto } from './dto/update-user-status.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth/jwt-auth.guard';
+import { RolesGuard } from 'src/common/guards/roles/roles.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
 
 @ApiTags('users')
 @Controller('users')
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
+  @Roles('admin')
   async findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
+  @Roles('admin', 'user')
   async findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
 
   @Post()
+  @Roles('admin')
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Patch('change-password')
+  @Roles('admin', 'user')
   async changePassword(
     @Req() req: any,
     @Body() changePasswordDto: ChangePasswordDto,
@@ -45,6 +54,7 @@ export class UsersController {
   }
 
   @Patch(':id/status')
+  @Roles('admin')
   async changeStatus(
     @Param('id') id: string,
     @Body() updateUserStatusDto: UpdateUserStatusDto,
@@ -53,11 +63,13 @@ export class UsersController {
   }
 
   @Patch(':id')
+  @Roles('admin', 'user')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
+  @Roles('admin')
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
   }
