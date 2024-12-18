91ab92a282845fd02dbf841db6c90c0acba1943d
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-18 20:55:43 +0330

    modify the name of update dto

--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateProductDto } from './create-product.dto';
 
-export class updateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto extends PartialType(CreateProductDto) {}
