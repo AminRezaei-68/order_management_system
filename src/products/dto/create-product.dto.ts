1dc51650cc1348b86dd416bf31eb69ce2e8da15f
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-18 19:52:30 +0330

    create product Dto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,11 +1,52 @@
-import { Schema } from '@nestjs/mongoose';
+import {
+  ArrayNotEmpty,
+  IsArray,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+} from 'class-validator';
+import { ProductCategory } from 'src/common/enums/category.enum';
+import { ProductStatus } from 'src/common/enums/status.enum';
 
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
-  description: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsNumber()
+  @IsPositive()
   price: number;
+
+  @IsNumber()
+  @IsPositive()
   quantity: number;
-  category: [string];
-  brand: string;
-  status: [string];
+
+  @IsArray()
+  @ArrayNotEmpty()
+  @IsEnum(ProductCategory, {
+    each: true,
+    message:
+      'Category must be either "clothing" or "electronics" or "home_appliances" or "jewelry".',
+  })
+  category: ProductCategory[];
+
+  @IsString()
+  @IsOptional()
+  brand?: string;
+
+  @IsArray()
+  @ArrayNotEmpty()
+  @IsEnum(ProductStatus, {
+    each: true,
+    message:
+      'status must be either "clothing" or "electronics" or "home_appliances" or "jewelry".',
+  })
+  status: ProductStatus[];
 }
