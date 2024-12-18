9f274a1b141c21cfcf6c6991fb714662d7177b7a
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-18 22:15:34 +0330

    create product servive file

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,103 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import mongoose, { Model } from 'mongoose';
+import { Product } from './schemas/product.schema';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PaginationQueryDto } from 'src/common/dtos/pagination-query.dto';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(
+    @InjectModel(Product.name) private readonly productModel: Model<Product>,
+  ) {}
+
+  async findAll(paginationQueryDto: PaginationQueryDto) {
+    const { limit, offset } = paginationQueryDto;
+    return await this.productModel.find().skip(offset).limit(limit).exec();
+  }
+
+  async findOne(id: string) {
+    if (!mongoose.isValidObjectId(id)) {
+      throw new BadRequestException('Invalid product ID');
+    }
+
+    const product = await this.productModel.findById(id).exec();
+
+    if (!product) {
+      throw new NotFoundException(`The product with ${id} does not exist.`);
+    }
+
+    return product;
+  }
+
+  async create(createProductDto: CreateProductDto) {
+    const { name, brand } = createProductDto;
+
+    const existingProduct = await this.productModel
+      .findOne({ name: name })
+      .exec();
+    if (existingProduct && existingProduct.brand === brand) {
+      throw new BadRequestException(`You can't create repetitive product`);
+    }
+
+    const product = new this.productModel({ ...createProductDto });
+    return await product.save();
+  }
+
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    if (!mongoose.isValidObjectId(id)) {
+      throw new BadRequestException('Invalid product ID');
+    }
+    const product = await this.productModel.findById(id).exec();
+
+    if (!product) {
+      throw new NotFoundException(`The product with ${id} does not exist.`);
+    }
+
+    if (updateProductDto.name) {
+      const existingName = await this.productModel
+        .findOne({
+          name: updateProductDto.name,
+        })
+        .exec();
+
+      if (existingName && existingName.brand === updateProductDto.brand) {
+        throw new BadRequestException(`You can't create repetitive product`);
+      }
+    }
+
+    if (updateProductDto.brand) {
+      const existingBrand = await this.productModel
+        .findOne({
+          brand: updateProductDto.brand,
+        })
+        .exec();
+      if (existingBrand && existingBrand.name === updateProductDto.name) {
+        throw new BadRequestException(`You can't create repetitive product`);
+      }
+    }
+
+    Object.assign(product, updateProductDto);
+    const updatedProduct = await product.save();
+
+    return updatedProduct;
+  }
+
+  async remove(id: string) {
+    if (!mongoose.isValidObjectId(id)) {
+      throw new BadRequestException('Invalid product ID');
+    }
+    const product = await this.productModel.findByIdAndDelete(id).exec();
+
+    if (!product) {
+      throw new NotFoundException(`The product with ${id} does not exist.`);
+    }
+
+    return { message: `Product with ID "${id}" has been deleted.` };
+  }
+}
