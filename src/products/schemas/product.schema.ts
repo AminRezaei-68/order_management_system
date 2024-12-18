04e8e98d50ca4b70f739af9f11b15ba592d000f3
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-18 19:56:43 +0330

    create schema for product

---
+++ src/products/schemas/product.schema.ts
@@ -0,0 +1,29 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema({ timestamps: true })
+export class Product extends Document {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop()
+  description?: string;
+
+  @Prop({ required: true })
+  price: number;
+
+  @Prop({ required: true })
+  quantity: number;
+
+  @Prop({ type: [String], required: true })
+  category: string[];
+
+  @Prop()
+  brand?: string;
+
+  @Prop({ type: [String], required: true })
+  status: string[];
+}
+
+export type ProductDocument = Product & Document;
+
+export const productSchema = SchemaFactory.createForClass(Product);
