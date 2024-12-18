83da7740acc097d9bece0149f0544425d00255db
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-18 23:32:09 +0330

    import user module in product module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,9 +3,11 @@ import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Product, productSchema } from './schemas/product.schema';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
+    UsersModule,
     MongooseModule.forFeature([{ name: Product.name, schema: productSchema }]),
   ],
   providers: [ProductsService],
