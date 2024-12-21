4956712ceaa28e168a43ed52ff0976b39e0d9423
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-21 22:41:28 +0330

    change userId to id in JwtStrategy

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
@@ -14,6 +14,9 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    return { userId: payload.sub, username: payload.username };
+    if (!payload || !payload.sub) {
+      throw new UnauthorizedException('Invalid token');
+    }
+    return { id: payload.sub, username: payload.username };
   }
 }
