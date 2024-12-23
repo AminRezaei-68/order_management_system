052f49aea3f07059dcd51d50d64ba37fe662989d
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-23 23:37:13 +0330

    add product to make model

--- src/carts/carts.module.ts
+++ src/carts/carts.module.ts
@@ -4,11 +4,15 @@ import { CartsService } from './carts.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Cart, CartSchema } from './schemas/cart.schema';
 import { UsersModule } from 'src/users/users.module';
+import { Product, productSchema } from 'src/products/schemas/product.schema';
 
 @Module({
   imports: [
     UsersModule,
-    MongooseModule.forFeature([{ name: Cart.name, schema: CartSchema }]),
+    MongooseModule.forFeature([
+      { name: Cart.name, schema: CartSchema },
+      { name: Product.name, schema: productSchema },
+    ]),
   ],
   controllers: [CartsController],
   providers: [CartsService],
