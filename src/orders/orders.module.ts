2041d2fd0c1a51475b40e682de5665a0ef80fb9d
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-29 00:05:17 +0330

    imports UsersModule and ProductsModule as circular dependency

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { ProductsModule } from 'src/products/products.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Order, orderSchema } from './schemas/order.schema';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  imports: [
+    UsersModule,
+    forwardRef(() => ProductsModule),
+    MongooseModule.forFeature([{ name: Order.name, schema: orderSchema }]),
+  ],
   providers: [OrdersService],
-  controllers: [OrdersController]
+  controllers: [OrdersController],
+  exports: [OrdersService],
 })
 export class OrdersModule {}
