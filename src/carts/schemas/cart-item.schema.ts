075adab7ad6187e1b2807befcb673f8b5203f53f
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-28 09:29:13 +0330

    add schema decorator to cart item

--- src/carts/schemas/cart-item.schema.ts
+++ src/carts/schemas/cart-item.schema.ts
@@ -1,6 +1,7 @@
-import { Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Schema as MongooseSchema } from 'mongoose';
 
+@Schema()
 export class CartItem {
   @Prop({ type: MongooseSchema.Types.ObjectId, ref: 'Product', required: true })
   productId: string;
