87336b38f701ff6f6f72dacbae8e9288592448db
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-29 00:01:16 +0330

    remove no need parameters and add guards to all route

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { OrdersService } from './orders.service';
+import { PaginationQueryDto } from 'src/common/dtos/pagination-query.dto';
+import { UpdateOrderStatusDto } from './dto/update-order-status.dto';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth/jwt-auth.guard';
+import { RolesGuard } from 'src/common/guards/roles/roles.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
 
+@ApiTags('orders')
 @Controller('orders')
-export class OrdersController {}
+@UseGuards(JwtAuthGuard, RolesGuard)
+export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) {}
+
+  @Get()
+  @Roles('admin')
+  async findAll(@Query() paginationQueryDto: PaginationQueryDto) {
+    return this.ordersService.findAll(paginationQueryDto);
+  }
+
+  @Get(':id')
+  @Roles('admin', 'user')
+  async findOne(@Param('id') id: string) {
+    return this.ordersService.findOne(id);
+  }
+
+  @Patch(':id')
+  @Roles('admin')
+  async update(
+    @Param('id') id: string,
+    @Body() updateOrderStatusDto: UpdateOrderStatusDto,
+  ) {
+    return this.ordersService.update(id, updateOrderStatusDto);
+  }
+
+  @Delete(':id')
+  @Roles('admin')
+  async remove(@Param('id') id: string) {
+    return this.ordersService.remove(id);
+  }
+}
