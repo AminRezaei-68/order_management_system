4fd5cf64de032c98994222ce6b5aeea7344448ae
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-28 09:39:08 +0330

    add checkout route cart controller

--- src/carts/carts.controller.ts
+++ src/carts/carts.controller.ts
@@ -6,13 +6,15 @@ import {
   Post,
   UseGuards,
   Request,
+  Get,
+  Query,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { CartsService } from './carts.service';
 import { CreateCartDto } from './dto/create-cart.dto';
-// import { UpdateCartDto } from './dto/update-cart.dto';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth/jwt-auth.guard';
 import { DecrementItemDto } from './dto/decrement-item.dto';
+import { PaginationQueryDto } from 'src/common/dtos/pagination-query.dto';
 
 @ApiTags('carts')
 @Controller('carts')
@@ -20,16 +22,24 @@ import { DecrementItemDto } from './dto/decrement-item.dto';
 export class CartsController {
   constructor(private readonly cartsService: CartsService) {}
 
+  @Get()
+  async findAll(@Query() paginationQueryDto: PaginationQueryDto) {
+    return this.cartsService.findAll(paginationQueryDto);
+  }
+
   @Post()
   async create(@Request() req, @Body() createCartDto: CreateCartDto) {
     const userId = req.user.id;
     return this.cartsService.create(userId, createCartDto);
   }
 
-  // @Patch(':id')
-  // async update(@Param('id') id: string, @Body() updateCartDto: UpdateCartDto) {
-  //   return this.cartsService.update(id, updateCartDto);
-  // }
+  @Post('checkout/:cartId')
+  async checkout(
+    @Param('cartId') cartId: string,
+    @Query('userId') userId: string,
+  ) {
+    return this.cartsService.checkout(cartId, userId);
+  }
 
   @Delete()
   async decrementItem(@Body() decrementItemDto: DecrementItemDto) {
