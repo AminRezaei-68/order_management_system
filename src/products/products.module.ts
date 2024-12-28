9f4ac0569325f7b722d1e173a441c448ec2ca119
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-28 23:56:35 +0330

    exports ProductsService &  MongooseModule in ProductsModule

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -4,13 +4,18 @@ import { ProductsController } from './products.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Product, productSchema } from './schemas/product.schema';
 import { UsersModule } from 'src/users/users.module';
+import { CartsModule } from 'src/carts/carts.module';
+import { OrdersModule } from 'src/orders/orders.module';
 
 @Module({
   imports: [
     UsersModule,
+    CartsModule,
+    OrdersModule,
     MongooseModule.forFeature([{ name: Product.name, schema: productSchema }]),
   ],
   providers: [ProductsService],
   controllers: [ProductsController],
+  exports: [ProductsService, MongooseModule],
 })
 export class ProductsModule {}
