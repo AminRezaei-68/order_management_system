648398edf8c3ffbd362518f3cef9b24944d4d8be
AminRezaei <rezaei.amin1268@gmail.com>
2025-01-04 08:50:26 +0330

    add forbidNonWhitelisted and transformOptions properties to ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,14 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
